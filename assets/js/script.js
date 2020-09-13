7ac3cf90a1b2d7234bb22c9c77c09a92bbc6ceb1
masayado <masayado@uc.cl>
2020-09-13 19:32:13 -0300

    agregue validacion input y para que solo se ingresen numeros

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
+//evento button
 $(function () {
     $("#buscar").click(e=>{
-        alert("buscando")
+
+        //validacion
+        var numeroPokemon = $("#input_pokemon").val();
+        var = regExNumbers = /^[0-9]*$/gm;
+        if(numeroPokemon===""){
+            alert("Input vacío. Ingresa un numero entre 1 y 807")
+            return false;
+        }
+        else if(numeroPokemon.match(regExNumbers)){
+            console.log(numeroPokemon);
+            return true;
+        }
+        else{
+            alert("Solo puedes ingresar números")
+            return false;
+        }
+
+        console.log(numeroPokemon);
     })
 });
+
+function getPokemon(id){
+
+}
