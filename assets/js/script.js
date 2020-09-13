72317253eca22162b3148e402db6b8a4ee36c315
masayado <masayado@uc.cl>
2020-09-13 20:41:50 -0300

    agregue funcion generarCard y funciona

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 //evento button
 $(function () {
     $("#buscar").click(e=>{
+        var numeroPokemon = $("#input_pokemon").val();
+
+        //traer personaje
+        getPokemon(numeroPokemon);
 
         //validacion
-        var numeroPokemon = $("#input_pokemon").val();
-        var = regExNumbers = /^[0-9]*$/gm;
-        if(numeroPokemon===""){
+        var regExNumbers = /^[0-9]*$/gm;
+        if(numeroPokemon === ""){
             alert("Input vacío. Ingresa un numero entre 1 y 807")
             return false;
         }
@@ -17,11 +20,35 @@ $(function () {
             alert("Solo puedes ingresar números")
             return false;
         }
-
-        console.log(numeroPokemon);
     })
 });
 
 function getPokemon(id){
+    $.ajax({
+        type: "GET",
+        url: `https://pokeapi.co/api/v2/pokemon/${id}/`,
+        success: function (response) {
+            console.log("response=>", response);
+            //imprimir data
+            generarCard(response);
+            $("#card").empty(generarCard(response));
+            $("#card").append(generarCard(response));
+        }
+    });
+}
 
+function generarCard(personaje){
+    var card = `
+    <div class="col-sm-12 col-md-4>
+        <div class="card" style="width: 100%;">
+            <img class="card-img-top" src="${personaje.sprites.front_default}" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title text-uppercase">${personaje.name}</h5>
+                <p class="card-text"><b>Tipo:</b>&nbsp;${personaje.types[0].type.name}</p>
+                <p class="card-text"><b>Habilidad:</b>&nbsp;${personaje.abilities[0].ability.name}</p>
+            </div>
+        </div>
+    </div>
+    `
+    return card;
 }
