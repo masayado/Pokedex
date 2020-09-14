b6c994af09bd37a2c7730dcb5701ba1cdd567573
masayado <masayado@uc.cl>
2020-09-13 21:14:33 -0300

    edite estilos del layout y card

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ $(function () {
         //validacion
         var regExNumbers = /^[0-9]*$/gm;
         if(numeroPokemon === ""){
-            alert("Input vacío. Ingresa un numero entre 1 y 807")
+            alert("Input vacío. Ingresa un numero entre 1 y 807.")
             return false;
         }
         else if(numeroPokemon.match(regExNumbers)){
@@ -17,7 +17,7 @@ $(function () {
             return true;
         }
         else{
-            alert("Solo puedes ingresar números")
+            alert("Solo puedes ingresar números.")
             return false;
         }
     })
@@ -39,16 +39,14 @@ function getPokemon(id){
 
 function generarCard(personaje){
     var card = `
-    <div class="col-sm-12 col-md-4>
-        <div class="card" style="width: 100%;">
-            <img class="card-img-top" src="${personaje.sprites.front_default}" alt="Card image cap">
+        <div class="card m-2" style="width: 100%;">
+            <img class="card-img-top" src="${personaje.sprites.front_default}" alt="${personaje.name}">
             <div class="card-body">
-                <h5 class="card-title text-uppercase">${personaje.name}</h5>
+                <h5 class="card-title text-uppercase text-center">${personaje.name}</h5>
                 <p class="card-text"><b>Tipo:</b>&nbsp;${personaje.types[0].type.name}</p>
                 <p class="card-text"><b>Habilidad:</b>&nbsp;${personaje.abilities[0].ability.name}</p>
             </div>
         </div>
-    </div>
     `
     return card;
 }
